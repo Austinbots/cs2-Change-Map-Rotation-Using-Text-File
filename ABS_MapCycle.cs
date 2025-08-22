284ae2f2ef84bd13394ce7f09e9cba380c767297
austin <sendjunkmailhere@resultsp.com>
2025-08-22 00:49:12 -0500

    Update ABS_MapCycle.cs
    V1.3
    Fixed a logic error where we were limiting having duplicate maps in the map list when not using random map selections.
    
    We now handle the match end delay settings to avoid the strange cs2 problem of trying to switch to a blank map when end of match voting is disabled.

--- ABS_MapCycle.cs
+++ ABS_MapCycle.cs
@@ -20,15 +20,14 @@ public class ABS_MapCycleConfig : BasePluginConfig
 public class ABS_MapCycle : BasePlugin, IPluginConfig<ABS_MapCycleConfig>
 {
 	public override string ModuleName => "ABS_MapCycle";
-	public override string ModuleVersion => "1.2";
+	public override string ModuleVersion => "1.3";
 	public override string ModuleAuthor => "austin";
 	public override string ModuleDescription => "Cycles server maps listed in a text file.";
 	public required ABS_MapCycleConfig Config {get; set;}
 
+	public string MapCycleFile;
 	public List<string> MapCycleList;
 	public List<string> MapCycleInUseList;
-
-	public string MapCycleFile;
 	public int MapCycleIdx = 0;
 
 	public override void	Load(bool hotReload)
@@ -38,6 +37,7 @@ public class ABS_MapCycle : BasePlugin, IPluginConfig<ABS_MapCycleConfig>
 		if(!Config.PluginEnabled)
 		{
 			Console.WriteLine($"Alert: ABS_MapCycle is set to Disabled. Map Cycle will be disabled.");
+			// returning before the RegisterEventHandler below effectively disables the plugin
 			return;
 		}
 
@@ -47,22 +47,43 @@ public class ABS_MapCycle : BasePlugin, IPluginConfig<ABS_MapCycleConfig>
 			Console.WriteLine($"WARNING: File not found = {MapCycleFile}\nABS_MapCycle will be disabled.");
 			return;
 		}
+
+		// create a list for the map cycle from the map cycle file
+		// remove blank lines and leading and trailing white spaces
+		// and for no reason lower case everything
 		MapCycleList = File.ReadAllLines(MapCycleFile).
 			Where(s => !string.IsNullOrWhiteSpace(s)).
 			Select(s => s.Trim()).
 			ToList().
 			ConvertAll(d => d.ToLower());
 
+		// show the map list including any comment lines
 		Console.WriteLine($"----------- Loaded {MapCycleFile} -----------");
 		Console.WriteLine($"EnableRandomMaps = {Config.EnableRandomMaps}");
 		Console.WriteLine($"EnableNoDuplicateRandomMaps = {Config.EnableNoDuplicateRandomMaps}");
+		Console.WriteLine($"MapCycleFile = {Config.MapCycleFile}");
 		foreach (string Map in MapCycleList)
 			Console.WriteLine($"{Map}");
 		Console.WriteLine("------------------------------------------------------------------");
 
+		// remove the comment lines
 		MapCycleList.RemoveAll(s => s.StartsWith(@"//"));
 
-		if (Config.EnableNoDuplicateRandomMaps)
+		// if we are in random map switching mode and disallowing dupliates
+		// remove any duplicate maps in the list
+		// then set the InUse list to this filtered list
+		// otherwise set the InUse list to the full list which allows duplicates
+		//
+		// we keep the full MapCycleList and the InUseList
+		// because if we are in random mode and no duplicates
+		// it is very easy to just remove the switched to maps 
+		// from the list to avoid picking them agian
+		// once all maps are run and the list is empty we "refill it"
+		// from the mapCycleList 
+		//
+		// if we are not in random mode the InUse is just cycled over and over
+		//
+		if (Config.EnableRandomMaps && Config.EnableNoDuplicateRandomMaps)
 			MapCycleInUseList = MapCycleList.Distinct().ToList();
 		else
 			MapCycleInUseList = MapCycleList.ToList();
@@ -84,30 +105,73 @@ public class ABS_MapCycle : BasePlugin, IPluginConfig<ABS_MapCycleConfig>
 	{
 		string NextMap;
 		int idx;
+
+		/*
+			after all rounds have played and the match has ended 
+			two time delays are used before the game switches the map
+			mp_win_panel_display_time
+			mp_match_restart_delay
+
+			if mp_win_panel_display_time > mp_match_restart_delay
+			You will see the win panel screen the entire time
+			and you will not see the map vote screen
+			Total delay in seconds after match end before the map switch will be
+			mp_win_panel_display_time 
+
+			if mp_match_restart_delay > mp_win_panel_display_time
+			You will see the win panel for mp_win_panel_display time
+			then you will see the map vote screen for match_restart_delay -  mp_win_panel_display_time
+			Total delay in seconds after match end before the map switch will be
+			mp_match_restart_delay 
+
+			We have to switch the map before this time expires because
+			if they have mp_endmatch_votenextmap 0
+			after the time expires the game will try to switch to a blank map 
+			and the server will either crash or go into a strange nonfunctioning state.
+		*/
+		
+		float WinPanelDelay;
+		int MatchRestartDelay;
 		float Delay;
-
-		var convar = ConVar.Find("mp_match_restart_delay");
-		if (convar is null)
-			Delay = 5.0f;
+		
+		var mp_win_panel_display_time = ConVar.Find("mp_win_panel_display_time");
+		if (mp_win_panel_display_time is null)
+			WinPanelDelay = 1;
 		else
-			Delay = convar.GetPrimitiveValue<int>();
+			WinPanelDelay = mp_win_panel_display_time.GetPrimitiveValue<float>();
 
+		var mp_match_restart_delay = ConVar.Find("mp_match_restart_delay");
+		if (mp_match_restart_delay is null)
+			MatchRestartDelay = 1;
+		else
+			MatchRestartDelay = mp_match_restart_delay.GetPrimitiveValue<int>();
+
+		Delay = Math.Max(WinPanelDelay, MatchRestartDelay);
+		// switch map 5 seconds before game tries to switch map or immediately if game is for no delay
+		Delay = Math.Max(0, Delay-5);
+	
 		if(Config.EnableRandomMaps)
 		{
 			idx = new Random().Next(0, MapCycleInUseList.Count - 1);
 			NextMap = MapCycleInUseList.ElementAtOrDefault(idx);
 
+			// to avoid changing to duplicate maps we remove maps from the UseList as they are used
 			if (Config.EnableNoDuplicateRandomMaps)
 				MapCycleInUseList.RemoveAt(idx);
 
+			// once we have been through all maps the list will be empty
+			// and we set it back to the full list read in orginally (that hasn't been modified)
 			if (MapCycleInUseList.Count() == 0)
 				if (Config.EnableNoDuplicateRandomMaps)
+					// we strip out dupes from the list for EnableNoDuplicateRandomMaps = true
 					MapCycleInUseList = MapCycleList.Distinct().ToList();
 				else
+					// we allow duplicate maps in the list even for random for EnableNoDuplicateRandomMaps = false
 					MapCycleInUseList = MapCycleList.ToList();
 		}
 		else
 		{
+			// we are not in ramdom. just keep cycling the InUseList in order
 			NextMap = MapCycleInUseList[MapCycleIdx];
 			if (++MapCycleIdx >= MapCycleInUseList.Count())
 				MapCycleIdx = 0;
@@ -118,7 +182,7 @@ public class ABS_MapCycle : BasePlugin, IPluginConfig<ABS_MapCycleConfig>
 		{
 			AddTimer(Delay, () =>
 			{
-				Console.WriteLine($"ABS_MapCycle Changing map to = {NextMap}");
+				Console.WriteLine($"ABS_MapCycle Changing map to = {NextMap} ==================================================================");
 				// if ulong parm assume workshop id
 				if (ulong.TryParse(NextMap, out _))
 					Server.ExecuteCommand($"host_workshop_map {NextMap}");
@@ -131,5 +195,3 @@ public class ABS_MapCycle : BasePlugin, IPluginConfig<ABS_MapCycleConfig>
 		return HookResult.Continue;
 	}
 }
-
-
